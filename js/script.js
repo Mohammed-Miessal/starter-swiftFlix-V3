e8f9685c901a68ccaa84fb096eb1a70cbabee019
Med <mohammedmiessal@gmail.com>
2023-10-22 15:55:18 +0100

    Form validation done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,122 @@
+
+//selecting elements
+let form = document.querySelector("#form");
+let userName = document.getElementById("fullname");
+let email = document.getElementById("email");
+let select = document.getElementById("topicSelect");
+let message = document.getElementById("message");
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    formValidation()
+})
+
+function formValidation(){
+    let userNameValue = userName.value;
+    let emailValue = email.value;
+    let selectValue = select.value;
+    let messageValue = message.value;
+
+    if(userNameValue === ''){
+        errorValidation(userName, "Full Name can not be blank")
+    }else{
+        successValidation(userName);
+    }
+
+    function errorValidation(input, message){
+        const formFlow = input.parentElement;
+        const small = formFlow.querySelector("small");
+
+        small.innerText = message;
+        formFlow.className = "form-flow error";
+
+    }
+    function successValidation(input, message){
+        const formFlow = input.parentElement;
+        const small = formFlow.querySelector("small");
+
+        small.innerText = message;
+        formFlow.className = "form-flow success";
+
+    }
+//////////////////////////////////////////////////////////////////
+
+    if(emailValue === ''){
+        errorValidation(email, "Email can not be blank")
+    }else{
+        successValidation(email);
+    }
+
+    function errorValidation(input, message){
+        const formFlow = input.parentElement;
+        const small = formFlow.querySelector("small");
+
+        small.innerText = message;
+        formFlow.className = "form-flow error";
+
+    }
+    function successValidation(input, message){
+        const formFlow = input.parentElement;
+        const small = formFlow.querySelector("small");
+
+        small.innerText = message;
+        formFlow.className = "form-flow success";
+
+    }
+/////////////////////////////////////////////////////
+
+    if(selectValue === 'Choose a Topic'){
+        errorValidation(select, "Select can not be blank")
+    }else{
+        successValidation(select);
+    }
+
+    function errorValidation(input, message) {
+        const formFlow = input.parentElement;
+        const select = input; // La balise select
+        const small = formFlow.querySelector("small");
+    
+        small.innerText = message;
+        formFlow.className = "form-flow error"; // Ajout de la classe "error" pour le style CSS
+    }
+
+    function successValidation(input, message){
+        const formFlow = input.parentElement;
+        const small = formFlow.querySelector("small");
+
+        small.innerText = message;
+        formFlow.className = "form-flow success";
+
+    }
+//////////////////////////////////////////////////////////////////
+
+    if(messageValue === ''){
+        errorValidation(message, "Message can not be blank")
+    }
+    else{
+        successValidation(message);
+    }
+
+    function errorValidation(input, message){
+        const formFlow = input.parentElement;
+        const small = formFlow.querySelector("small");
+
+        small.innerText = message;
+        formFlow.className = "form-flow error";
+
+    }
+    function successValidation(input, message){
+        const formFlow = input.parentElement;
+        const small = formFlow.querySelector("small");
+
+        small.innerText = message;
+        formFlow.className = "form-flow success";
+
+    }
+
+}
+// ----------------------------------------------------------------------
+
 function myMap() {
     var mapProp = {
         center: new google.maps.LatLng(40.712775, -74.005973),
